a888be20db05bcf6d28dc5f2ef1619f38a796dc2
Katsuma Narisawa <face.the.music721@gmail.com>
2019-02-10 21:52:16 +0900

    Remove Dashboard

--- components/index.js
+++ components/index.js
@@ -1,12 +1,10 @@
 import Breadcrumb from './Breadcrumb.vue'
-import Callout from './Callout.vue'
 import Header from './Header/Header.vue'
 import Sidebar from './Sidebar/Sidebar.vue'
 import Switch from './Switch.vue'
 
 export {
   Breadcrumb,
-  Callout,
   Header,
   Sidebar,
   Switch
