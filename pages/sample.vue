4b3d4371dba76c8b828b4109ef52e34a74bd9957
Katsuma Narisawa <face.the.music721@gmail.com>
2019-02-10 23:31:38 +0900

    Remove unnecessaries

--- pages/sample.vue
+++ pages/sample.vue
@@ -2,24 +2,9 @@
   div
     .animated.fadeIn
       b-row
-        b-col(lg='6')
+        b-col(lg='12')
           c-table(caption="<i class='fa fa-align-justify'></i> Simple Table")
         // /.col
-        b-col(lg='6')
-          c-table(striped='', caption="<i class='fa fa-align-justify'></i> Striped Table")
-        // /.col
-        // /.row
-        b-row
-          b-col(lg='6')
-            c-table(small='', caption="<i class='fa fa-align-justify'></i> Condensed Table")
-          // /.col
-          b-col(lg='6')
-            c-table(fixed='', bordered='', caption="<i class='fa fa-align-justify'></i> Bordered Table")
-          // /.col
-          // /.row
-          b-row
-            b-col(sm='12')
-              c-table(hover='', striped='', bordered='', small='', fixed='', caption="<i class='fa fa-align-justify'></i> Combined All Table")
 </template>
 
 <script>
