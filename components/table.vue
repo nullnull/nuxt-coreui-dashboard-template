b91084b1d1c9f68ff8821334fbca3173bdec16ea
Katsuma Narisawa <face.the.music721@gmail.com>
2019-02-11 16:14:19 +0900

    Style up

--- components/table.vue
+++ components/table.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  b-card(:header='caption')
+  div
     b-table(:hover='hover', :striped='striped', :bordered='bordered', :small='small', :fixed='fixed', responsive='sm', :items='items', :fields='fields', :current-page='currentPage', :per-page='perPage')
       template(slot='status', slot-scope='data')
         b-badge(:variant='getBadge(data.item.status)') {{data.item.status}}
